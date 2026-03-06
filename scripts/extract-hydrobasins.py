9e84117b42a059ef145b3951c578cbb92c7bdef8
gllamat <62991426+gllamat@users.noreply.github.com>
2026-03-06 13:38:57 +0000

    update basins

--- scripts/extract-hydrobasins.py
+++ scripts/extract-hydrobasins.py
@@ -14,7 +14,7 @@ def main():
           "properties": { "HYBAS_ID": 7060451, "PFAF_ID": 762410, "risk_class": 3, "school_count": 0, "schools": "" },
           "geometry": {
             "type": "Polygon",
-            "coordinates": [[[-103.5, 20.5], [-103.2, 20.5], [-103.2, 20.8], [-103.5, 20.8], [-103.5, 20.5]]]
+            "coordinates": [[[-103.5, 20.5], [-103.3, 20.4], [-103.1, 20.5], [-103.1, 20.7], [-103.2, 20.9], [-103.5, 20.8], [-103.6, 20.7], [-103.5, 20.5]]]
           }
         },
         {
@@ -22,15 +22,15 @@ def main():
           "properties": { "HYBAS_ID": 7060482, "PFAF_ID": 762420, "risk_class": 4, "school_count": 0, "schools": "" },
           "geometry": {
             "type": "Polygon",
-            "coordinates": [[[-100.5, 20.5], [-100.2, 20.5], [-100.2, 20.8], [-100.5, 20.8], [-100.5, 20.5]]]
+            "coordinates": [[[-100.6, 20.5], [-100.4, 20.4], [-100.2, 20.5], [-100.1, 20.7], [-100.3, 20.9], [-100.5, 20.8], [-100.6, 20.6], [-100.6, 20.5]]]
           }
         },
         {
           "type": "Feature",
-          "properties": { "HYBAS_ID": 7060073910, "PFAF_ID": 753240, "risk_class": 2, "school_count": 4, "schools": "SECUNDARIA, PRIMARIA, etc" },
+          "properties": { "HYBAS_ID": 7060073910, "PFAF_ID": 753240, "risk_class": 2, "school_count": 6, "schools": "BENITO JUAREZ, ZAPATA, PREPA 271, VICENTE GUERRERO, IGNACIO ALLENDE, FLORES MAGON" },
           "geometry": {
             "type": "Polygon",
-            "coordinates": [[[-98.5, 19.8], [-98.2, 19.8], [-98.2, 20.1], [-98.5, 20.1], [-98.5, 19.8]]]
+            "coordinates": [[[-99.2, 19.0], [-98.7, 19.0], [-98.6, 19.3], [-98.7, 19.6], [-99.1, 19.7], [-99.3, 19.4], [-99.2, 19.0]]]
           }
         }
       ]
